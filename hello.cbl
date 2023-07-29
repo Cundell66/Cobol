6a2d73e14cf48edc11a64a24ba44ba9712a3319b
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-07-30 00:10:42 +0100

    cobol

---
+++ hello.cbl
@@ -0,0 +1,21 @@
+      *****************************************************************
+      * Program name:    MYPROG                               
+      * Original author: MYNAME                                
+      *
+      * Maintenence Log                                              
+      * Date      Author        Maintenance Requirement               
+      * --------- ------------  --------------------------------------- 
+      * 01/01/08 MYNAME  Created for COBOL class         
+      *                                                               
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  MYPROG.
+       AUTHOR. MYNAME. 
+       INSTALLATION. COBOL DEVELOPMENT CENTER. 
+       DATE-WRITTEN. 01/01/08. 
+       DATE-COMPILED. 01/01/08. 
+       SECURITY. NON-CONFIDENTIAL.
+      *****************************************************************
+             ******************************************************************
+              PROCEDURE DIVISION.
+                 DISPLAY "Hello World"
